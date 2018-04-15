b82e8a0b501432b45c05af29bebd0f850e37de5c
Rohit Singh <rohitsingh17219@gmail.com>
2018-04-15 15:33:06 +0530

    Added Browserstack local testing config and wait time

--- features/src/test/java/com/singh/configuration/BrowserStackDriverConfiguration.java
+++ features/src/test/java/com/singh/configuration/BrowserStackDriverConfiguration.java
@@ -9,6 +9,8 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+
 /**
  * File: BrowserStackDriverConfiguration
  * Created by Rohit Singh on 15-04-2018.
@@ -23,6 +25,9 @@ public class BrowserStackDriverConfiguration {
     public static WebDriver setUpDriver() throws BrowserStackDriverInitializationException {
         try {
             DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("browserstack.local", "true");
+            caps.setCapability("browserstack.debug", "true");
+            caps.setCapability("browserstack.video", "true");
             caps.setCapability("browser", "Chrome");
             caps.setCapability("browser_version", "44.0");
             caps.setCapability("os", "Windows");
@@ -30,6 +35,7 @@ public class BrowserStackDriverConfiguration {
             caps.setCapability("resolution", "1280x1024");
 
             RemoteWebDriver driver = new RemoteWebDriver(new URL(BROWSER_STACK_URL), caps);
+            driver.manage().timeouts().implicitlyWait(30, SECONDS);
             return driver;
         } catch (MalformedURLException | WebDriverException e) {
             throw new BrowserStackDriverInitializationException("Failed to load BroserStack remote driver!");
